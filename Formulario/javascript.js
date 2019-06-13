c859cd453ab1cef6f99a8e269d32028d17cfbaab
Michael Cervera <michaelcervera13@gmail.com>
2019-06-13 18:48:27 -0500

    first commit

---
+++ Formulario/javascript.js
@@ -0,0 +1,13 @@
+var inputs = document.getElementsByClassName('Formulario_Input');
+for (var i = 0; i < inputs.length; i++) {
+    
+    inputs[i].addEventListener('keyup', function(){
+        if (this.value.length>=1)   {
+       this.nextElementSibling.classList.add('Fijar');
+   
+}else{
+     this.nextElementSibling.classList.remove('Fijar');
+       }  
+        
+       });
+}
